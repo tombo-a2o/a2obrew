6102b3c21c3bdff17c6a4277a9386dcf4c9eb83a
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-04-24 20:07:43 +0900

    Add script to do something same to emscripten repos
    For example:
      ./scripts/do_all.sh git checkout feature/something

---
+++ scripts/do_all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "Usage: ./do_all.sh [some commands]"
+  exit 1
+fi
+
+for repo in emsdk/emscripten/a2o_64bit emsdk/clang/fastcomp/src emsdk/clang/fastcomp/src/tools/clang; do
+  (cd $repo; pwd; $*)
+done
