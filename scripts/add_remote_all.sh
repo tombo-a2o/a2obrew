83a0f6c37ad6e5bcebb81dc0489fc380e025f472
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-04-24 19:59:42 +0900

    Add utility script to add and fetch remote repos for emscripten repos

---
+++ scripts/add_remote_all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -ex
+
+# needs user id
+[ $# -eq 1 ] 
+
+for repo in emsdk/emscripten/a2o_64bit emsdk/clang/fastcomp/src emsdk/clang/fastcomp/src/tools/clang; do
+  pushd $repo
+  remote=`git config --get remote.origin.url`
+  add=`echo $remote | sed -e s/tomboinc/$1/g`
+  git remote show $1 || git remote add $1 $add
+  git fetch $1
+  popd
+done
