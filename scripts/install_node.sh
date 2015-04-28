ce776cc978bc68764a387f59a41af30391d588d4
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-04-28 19:20:45 +0900

    Use system nvm if available

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -1,15 +1,17 @@
 #/bin/bash -exu
 # Install Node.js woth nvm
-if [ ! -d ${HOME}/.nvm ]; then
-  curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
-  source ${HOME}/.nvm/nvm.sh > /dev/null 2>&1
-  nvm install 0.12.2
+
+if which nvm > /dev/null; then
+  echo "* nvm has already been installed"
 else
-  echo "* nvm is installed"
-  source ${HOME}/.nvm/nvm.sh
-  if nvm ls 0.12.2 > /dev/null; then
-    echo "* node.js 0.12.2 with nvm is installed"
-  else
-    nvm install 0.12.2
+  if [ ! -d ${HOME}/.nvm ]; then
+    curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
   fi
+  source ${HOME}/.nvm/nvm.sh > /dev/null 2>&1
+fi
+
+if nvm ls 0.12.2 > /dev/null; then
+  echo "* node.js 0.12.2 with nvm has already been installed"
+else
+  nvm install 0.12.2
 fi
