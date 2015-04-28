07de97bdcf8a9bbef6e2e283c97cd4945509dd23
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-04-28 18:09:58 +0900

    add pyenv loader to .bash_profile

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -2,6 +2,9 @@
 # Install Python2 with pyenv
 if [ ! -d ~/.pyenv ]; then
   curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+  echo "export PATH=\"\$HOME/.pyenv/bin:\$PATH\"" >> ~/.bash_profile
+  echo "eval \"\$(pyenv init -)\"" >> ~/.bash_profile
+  echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.bash_profile
   export PATH="$HOME/.pyenv/bin:$PATH"
   eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
