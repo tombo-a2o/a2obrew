ac72b2a9f7f96657184cb89319638d477151fe73
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-04-28 19:47:58 +0900

    Fix nvm path on CircleCI

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -1,13 +1,14 @@
 #/bin/bash -exu
 # Install Node.js woth nvm
 
-if which nvm > /dev/null; then
+if nvm > /dev/null; then
   echo "* nvm has already been installed"
 else
-  if [ ! -d ${HOME}/.nvm ]; then
-    curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
+  if [ -z "$NVM_DIR" ]; then
+    NVM_DIR="${HOME}/.nvm"
   fi
-  source ${HOME}/.nvm/nvm.sh > /dev/null 2>&1
+  curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
+  source ${NVM_DIR}/nvm.sh > /dev/null 2>&1
 fi
 
 if nvm ls 0.12.2 > /dev/null; then
