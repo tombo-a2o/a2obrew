b94ea6a21c3d0e7659234da72035a71b813d2504
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-04-28 14:05:25 +0900

    libdispatch linux with emscripten. It doesn't work yet.

--- scripts/build_libdispatch.sh
+++ scripts/build_libdispatch.sh
@@ -1,12 +1,33 @@
 #!/bin/sh -ex
 cd emsdk; source ./emsdk_env.sh > /dev/null; cd ..
 
-if [ ! -d ./libdispatch ]; then
-  git clone git@github.com:tomboinc/libdispatch.git --branch feature/emscripten
+if [ ! -d ./blocks-runtime ]; then
+  git clone git@github.com:mheily/blocks-runtime.git
 fi
 
-cd libdispatch
+if [ ! -d ./libkqueue ]; then
+  git clone git@github.com:tomboinc/libkqueue.git --branch feature/emscripten
+fi
 
-autoreconf -i
-emconfigure ./configure
-make -f Makefile.dummy install
+if [ ! -d ./libpwq ]; then
+  git clone git@github.com:tomboinc/libpwq.git --branch feature/emscripten
+fi
+
+if [ ! -d ./libdispatch-linux ]; then
+  git clone git@github.com:tomboinc/libdispatch-linux.git --branch feature/emscripten
+fi
+
+for repo in blocks-runtime libkqueue libpwq; do
+  pushd $repo
+  autoreconf -i || autoreconf -i
+  emconfigure ./configure --prefix=${EMSCRIPTEN}/system/local
+  emmake make
+  emmake make install
+  popd
+done
+
+cd libdispatch-linux
+# No need to call emconfigure because the configure script is just a wrapper of cmake and calls emcmake internally
+./configure --prefix=${EMSCRIPTEN}/system/local
+make
+make install
