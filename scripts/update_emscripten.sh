6a3a492bc30f1bad5f54ec1d64f0275f518260c3
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-06-25 02:17:36 +0900

    Maximize llvm build speed

--- scripts/update_emscripten.sh
+++ scripts/update_emscripten.sh
@@ -2,4 +2,11 @@
 (cd emsdk/emscripten/a2o; git pull)
 (cd emsdk/clang/fastcomp/src; git pull)
 (cd emsdk/clang/fastcomp/src/tools/clang; git pull)
-(cd emsdk/clang/fastcomp/build_feature-objc_64; make)
+if type nproc ; then
+  JOBS=`nproc`
+elif type gnproc ; then
+  JOBS=`gnproc`
+else
+  JOBS=1
+fi
+(cd emsdk/clang/fastcomp/build_feature-objc_64; make -j${JOBS})
