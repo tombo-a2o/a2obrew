1317de74dd2ae4c345980ec8e8b42f7c8edcc75c
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-06-24 23:51:53 +0900

    libtool is also needed!!!

--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install base softwares
 sudo aptitude update
-sudo aptitude install -y build-essential git-core cmake default-jre autoconf \
+sudo aptitude install -y build-essential git-core cmake default-jre autoconf libtool \
   wget curl \
   libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev
