3130c6bb99051d9e88095bb160cc432c83fb618f
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-06-25 18:10:19 +0900

    Install pyenv to zsh

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -2,9 +2,22 @@
 # Install Python2 with pyenv
 if [ ! -d ${HOME}/.pyenv ]; then
   curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-  echo "export PATH=\"\$HOME/.pyenv/bin:\$PATH\"" >> ${HOME}/.bash_profile
-  echo "eval \"\$(pyenv init -)\"" >> ~/.bash_profile
-  echo "eval \"\$(pyenv virtualenv-init -)\"" >> ${HOME}/.bash_profile
+  case "${SHELL}" in
+  *"bash")
+    echo "export PATH=\"\$HOME/.pyenv/bin:\$PATH\"" >> ${HOME}/.bash_profile
+    echo "eval \"\$(pyenv init -)\"" >> ~/.bash_profile
+    echo "eval \"\$(pyenv virtualenv-init -)\"" >> ${HOME}/.bash_profile
+    ;;
+  *"zsh")
+    echo "export PATH=\"\$HOME/.pyenv/bin:\$PATH\"" >> ${HOME}/.zshrc
+    echo "eval \"\$(pyenv init -)\"" >> ~/.zshrc
+    echo "eval \"\$(pyenv virtualenv-init -)\"" >> ${HOME}/.zshrc
+    ;;
+  *)
+    echo "Unknown shell"
+    exit 1
+    ;;
+  esac
   export PATH="$HOME/.pyenv/bin:$PATH"
   eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
