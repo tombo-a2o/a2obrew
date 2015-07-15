59e627fa729d37c958eeffb9e7877d92cd8d564e
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-07-15 19:35:24 +0900

    Add Chameleon to ignore and cleanup list.

--- scripts/clean_libraries.sh
+++ scripts/clean_libraries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -exu
 
-rm -rf Foundation blocks-runtime icu libbsd libdispatch-linux libkqueue libpwq objc4
+rm -rf Foundation blocks-runtime icu libbsd libdispatch-linux libkqueue libpwq objc4 Chameleon
 cd emsdk/emscripten/a2o/system
 git clean -fdx .
 git checkout .
