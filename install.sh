ed5111924ff15a87a6e44f2f8a255f82d64b21c1
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-10-29 05:28:44 -0700

    Add openssl

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ fi
 ./scripts/build_ICU.sh
 ./scripts/build_libdispatch_em.sh
 ./scripts/build_cairo.sh
+./scripts/build_openssl.sh
 
 ./scripts/build_frameworks.sh install_header_only
 ./scripts/build_frameworks.sh install
---
+++ scripts/build_openssl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+source emsdk/emsdk_env.sh > /dev/null
+
+if [ ! -d ./openssl ]; then
+  git clone git@github.com:gunyarakun/openssl.git --branch feature/emscripten
+fi
+
+for repo in openssl; do
+  pushd $repo
+  git pull
+  emmake sh ./Configure -no-asm no-ssl3 no-comp no-hw no-engine no-deprecated no-shared no-dso no-gmp --openssldir=$EMSCRIPTEN/system/local linux-generic32
+  emmake make build_libs
+  emmake make install_emscripten
+  popd
+done
