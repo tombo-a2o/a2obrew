65706d2fab30c2d99879b26fbf0a5d2583721a45
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-11-04 00:25:47 -0800

    Update a2obrew itself by a2obrew update

--- bin/lib/a2obrew/cli.rb
+++ bin/lib/a2obrew/cli.rb
@@ -32,55 +32,59 @@ module A2OBrew
     desc 'update', 'update a2obrew and all dependent repositories'
     def update
       depends = A2OCONF[:depends]
-      depends[:projects].each{|proj|
+      depends[:projects].each {|proj|
         proj_path = "#{depends[:path]}/#{proj[:path]}"
-
-        if File.directory?(proj_path)
-          # git clone has already done
-
-          git_path = "#{proj_path}/.git"
-          git_command = "git --git-dir=#{git_path} --work-tree=#{proj_path}"
-
-          # Change current branch if needed
-          current_branch = `#{git_command} rev-parse --abbrev-ref HEAD`
-
-          if proj[:branch] and current_branch != proj[:branch]
-            puts `#{git_command} checkout #{proj[:branch]}`
-          end
-
-          # check the repository is up to date or not
-          puts `#{git_command} remote update`
-          local = `#{git_command} rev-parse @`
-          remote = `#{git_command} rev-parse @{u}`
-          base = `#{git_command} merge-base @ @{u}`
-
-          if local == remote
-            puts "#{proj_path} is up to date."
-          elsif local == base
-            # git pull if needed
-            puts `#{git_command} pull`
-            if $CHILD_STATUS != 0
-              error_exit "git pull fails on #{proj_path}"
-            end
-          elsif remote == base
-            error_exit "You must do 'git push' on #{proj_path}"
-          else
-            # diverged
-            error_exit "You must update #{proj_path} yourself"
-          end
-        else
-          # git clone hasn't done yet, so do git clone
-          if proj[:branch]
-            branch_option = "--branch #{proj[:branch]}"
-          else
-            branch_option = ''
-          end
-          puts `git clone #{proj[:repository_uri]} #{branch_option} #{proj_path}`
-        end
+        git_update(proj_path, proj[:branch], proj[:repository_uri])
       }
+      git_update(a2obrew_path, nil, nil)
     end
 
     private
+
+    def git_update(root_path, branch_name, repository_uri)
+      if File.directory?(root_path)
+        # git clone has already done
+
+        git_path = "#{root_path}/.git"
+        git_command = "git --git-dir=#{git_path} --work-tree=#{root_path}"
+
+        # Change current branch if needed
+        current_branch = `#{git_command} rev-parse --abbrev-ref HEAD`
+
+        if branch_name and current_branch != branch_name
+          puts `#{git_command} checkout #{branch_name}`
+        end
+
+        # check the repository is up to date or not
+        puts `#{git_command} remote update`
+        local = `#{git_command} rev-parse @`
+        remote = `#{git_command} rev-parse @{u}`
+        base = `#{git_command} merge-base @ @{u}`
+
+        if local == remote
+          puts "#{root_path} is up to date."
+        elsif local == base
+          # git pull if needed
+          puts `#{git_command} pull`
+          if $CHILD_STATUS != 0
+            error_exit "git pull fails on #{root_path}"
+          end
+        elsif remote == base
+          error_exit "You must do 'git push' on #{root_path}"
+        else
+          # diverged
+          error_exit "You must update #{root_path} yourself"
+        end
+      else
+        # git clone hasn't done yet, so do git clone
+        if branch_name
+          branch_option = "--branch #{branch_name}"
+        else
+          branch_option = ''
+        end
+        puts `git clone #{repository_uri} #{branch_option} #{root_path}`
+      end
+    end
 
     def a2obrew_path
       File.expand_path('../../../..', __FILE__)
