1a0f7f5d2f36450c986089a9b2d3b622f366c479
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-11-07 20:23:01 +0900

    Improve ruby install script

--- scripts/install_ruby.sh
+++ scripts/install_ruby.sh
@@ -3,14 +3,33 @@
 
 if [ -d ${HOME}/.rbenv ]; then
   echo "* rbenv has already been installed"
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
 else
   git clone https://github.com/sstephenson/rbenv.git ${HOME}/.rbenv
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
+  case "${SHELL}" in
+  *"bash")
+    echo "export PATH=\"\$HOME/.rbenv/bin:\$PATH\"" >> ${HOME}/.bash_profile
+    echo "eval \"\$(rbenv init -)\"" >> ~/.bash_profile
+    ;;
+  *"zsh")
+    echo "export PATH=\"\$HOME/.rbenv/bin:\$PATH\"" >> ${HOME}/.zshrc
+    echo "eval \"\$(rbenv init -)\"" >> ~/.zshrc
+    ;;
+  *)
+    echo "Unknown shell"
+    exit 1
+    ;;
+  esac
 fi
 
 if [ -d ${HOME}/.rbenv/plugins/ruby-build ]; then
   echo "* ruby-build has already been installed"
+  if ! rbenv install --list | grep " 2\\.2\\.2" > /dev/null; then
+    cd ${HOME}/.rbenv/plugins/ruby-build && git pull
+  fi
 else
   git clone git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 fi
@@ -19,10 +38,14 @@ if [ -d ${HOME}/.rbenv/plugins/rbenv-gemset ]; then
   echo "* rbenv-gemset has already been installed"
 else
   git clone git://github.com/jf/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
+fi
+
+if ! gem list --local | grep "bundler " > /dev/null; then
   gem install bundler
   rbenv rehash
-  bundle install
 fi
+
+bundle install
 
 if rbenv versions --bare | grep "2\\.2\\.2" > /dev/null; then
   echo "* Ruby 2.2.2 with rbenv has already been installed"
