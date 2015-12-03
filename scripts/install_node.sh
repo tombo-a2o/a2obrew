c3ed092e0ab9e1e235b5758911816d749cef5fca
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-12-02 20:02:38 -0800

    Weaken some shebang options

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -exu
+#!/bin/bash
 # Install Node.js with nvm
 
 if [ -z "$NVM_DIR" ]; then
@@ -6,25 +6,12 @@ if [ -z "$NVM_DIR" ]; then
 fi
 
 if [ ! -e "$NVM_DIR" ]; then
-  curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
-
-  case "${SHELL}" in
-  *"bash")
-    echo "[[ -s \"\${HOME}/.nvm/nvm.sh\" ]] && source \${HOME}/.nvm/nvm.sh" >> ${HOME}/.bash_profile
-    ;;
-  *"zsh")
-    echo "[[ -s \"\${HOME}/.nvm/nvm.sh\" ]] && source \${HOME}/.nvm/nvm.sh" >> ${HOME}/.zshrc
-    ;;
-  *)
-    echo "Unknown shell"
-    exit 1
-    ;;
-  esac
+  curl https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 fi
 
-source ${NVM_DIR}/nvm.sh > /dev/null
+source ${NVM_DIR}/nvm.sh
 
-if nvm ls 0.12.2 > /dev/null; then
+if ! nvm ls 0.12.2 > /dev/null; then
   echo "* node.js 0.12.2 with nvm has already been installed"
 else
   nvm install 0.12.2
