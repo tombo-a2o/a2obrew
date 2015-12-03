296732aceeb44c60e4e50a3e39a8aadf67805179
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-12-02 18:08:24 -0800

    Improve install Node.js scripte

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -exu
 # Install Node.js with nvm
 
-if nvm > /dev/null; then
-  echo "* nvm has already been installed"
-else
-  if [ -z "$NVM_DIR" ]; then
-    NVM_DIR="${HOME}/.nvm"
-  fi
-  if [ ! -e "$NVM_DIR" ]; then
-    curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
-  fi
+if [ -z "$NVM_DIR" ]; then
+  NVM_DIR="${HOME}/.nvm"
+fi
+
+if [ ! -e "$NVM_DIR" ]; then
+  curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
 
   case "${SHELL}" in
   *"bash")
@@ -24,6 +21,7 @@ else
     ;;
   esac
 fi
+
 source ${NVM_DIR}/nvm.sh > /dev/null 2>&1
 
 if nvm ls 0.12.2 > /dev/null; then
