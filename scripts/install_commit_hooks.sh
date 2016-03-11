dd184762bf083a30407bbc543094fb672a00a2f1
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-03-10 23:28:15 -0800

    Check pre-commit is already installed or not

--- scripts/install_commit_hooks.sh
+++ scripts/install_commit_hooks.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-if [ -d '.git/hooks' ]; then
+if [ -d '.git/hooks' ] && [ ! -f '.git/hooks/pre-commit' ] ; then
   ln -s ../../git-hooks/pre-commit .git/hooks
+  echo 'git pre-commit hook is installed'
 else
-  echo "Cannot find .git/hooks"
-  exit 1
+  echo 'git pre-commit hook is already installed'
 fi
