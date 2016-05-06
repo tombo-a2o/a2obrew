c2db931a30ce6792a03043d0937aedeae8ecea9c
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-05-06 19:03:25 +0900

    Add .tombo/config generator

--- bin/lib/a2obrew/dotfile.rb
+++ bin/lib/a2obrew/dotfile.rb
@@ -1,4 +1,5 @@
 require 'inifile'
+require 'highline'
 
 module A2OBrew
   class Dotfile
@@ -6,13 +7,21 @@ module A2OBrew
       @base_path = checked_base_path(base_path)
 
       section = profile_name.nil? ? 'default' : "profile #{profile_name}"
-      @config_path = checked_config_path(File.join(@base_path, 'config'))
+      @config_path = checked_config_path(File.join(@base_path, 'config'), section)
 
       @config = IniFile.load(@config_path)[section]
     end
 
-    def developer_portal_uri
-      @config['developer_portal_uri']
+    def developer_portal_uri(path)
+      @config['developer_portal_uri'] + path
+    end
+
+    def developer_credential_id
+      @config['developer_credential_id']
+    end
+
+    def developer_credential_secret
+      @config['developer_credential_secret']
     end
 
     private
@@ -27,16 +36,28 @@ module A2OBrew
       base_path
     end
 
-    def checked_config_path(config_path)
-      unless File.exist?(config_path)
-        File.open(config_path, 'w') do |f|
-          f.puts <<EOF
-[default]
-developer_portal_uri = https://developer.tom.bo/
-EOF
-        end
-      end
+    def checked_config_path(config_path, section)
+      generate_config(config_path, section) unless File.exist?(config_path)
       config_path
+    end
+
+    def generate_config(config_path, section) # rubocop:disable Metrics/MethodLength
+      puts "Generate #{config_path} to save platform informations"
+      hl = HighLine.new
+      dev_portal_uri = hl.ask('Developer portal URI:') do |q|
+        q.default = 'https://developer.tom.bo'
+      end
+      dev_credential_id = hl.ask('Developer credential id:')
+      dev_credential_secret = hl.ask('Developer credential secret:')
+
+      File.open(config_path, 'w') do |f|
+        f.puts <<EOF
+[#{section}]
+developer_portal_uri = #{dev_portal_uri}
+developer_credential_id = #{dev_credential_id}
+developer_credential_secret = #{dev_credential_secret}
+EOF
+      end
     end
   end
 end
