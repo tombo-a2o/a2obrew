544f8828c470432b47c9e0e062f5b55b10feb2b7
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-05-29 18:30:33 -0700

    Add `--memory-init-file 1` for emcc html generation

--- bin/lib/a2obrew/xcode2ninja.rb
+++ bin/lib/a2obrew/xcode2ninja.rb
@@ -531,7 +531,7 @@ module A2OBrew
       rules << {
         rule_name: 'html',
         description: 'generate executables: ${out}',
-        command: "EMCC_DEBUG=1 EMCC_DEBUG_SAVE=1 a2o -v ${framework_options} ${lib_options} ${separate_asm_options} -s VERBOSE=1 -s LZ4=1 -s NATIVE_LIBDISPATCH=1 -o #{html_path(target, a2o_target)} ${linked_objects} --pre-js ${data_js} ${shared_library_options} -licuuc -licui18n #{conf_html_flags}"
+        command: "EMCC_DEBUG=1 EMCC_DEBUG_SAVE=1 a2o -v ${framework_options} ${lib_options} ${separate_asm_options} -s VERBOSE=1 -s LZ4=1 -s NATIVE_LIBDISPATCH=1 -o #{html_path(target, a2o_target)} ${linked_objects} --pre-js ${data_js} ${shared_library_options} -licuuc -licui18n --memory-init-file 1 #{conf_html_flags}"
       }
 
       builds << {
