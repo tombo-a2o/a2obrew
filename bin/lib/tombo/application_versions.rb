fbf6daab26a6771936784726d09358dac8c5cb47
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-06-08 19:22:29 -0700

    Fix typo

--- bin/lib/tombo/application_versions.rb
+++ bin/lib/tombo/application_versions.rb
@@ -35,7 +35,7 @@ module Tombo
 
       d = json['data']
 
-      if d.nil? || d['type'] != 'application_verions' || d['id'].nil?
+      if d.nil? || d['type'] != 'application_versions' || d['id'].nil?
         raise 'Cannot create application version'
       end
 
