64480ebd221f43b52204dbb96bf153a90f16884e
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-06-08 19:49:05 -0700

    Add an applications subcommand

---
+++ bin/lib/tombo/applications.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+require 'json'
+require_relative 'dotfile'
+require_relative 'cli_base'
+
+module Tombo
+  class Applications < CLIBase
+    desc 'list', 'application list'
+    method_option :profile, aliases: '-p', desc: 'Profile name for Tombo Platform'
+    def list
+      json = request('GET', '/applications.json')
+
+      d = json['data']
+
+      raise 'Cannot get application list' if d.nil? || !d.is_a?(Array)
+
+      output(d)
+    end
+  end
+end
--- bin/lib/tombo/cli.rb
+++ bin/lib/tombo/cli.rb
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 require_relative 'cli_base'
+require_relative 'applications'
 require_relative 'application_versions'
 
 module Tombo
@@ -10,6 +11,9 @@ module Tombo
     def commands
       self.class.puts_commands
     end
+
+    desc 'applications SUBCOMMAND', 'handle applications'
+    subcommand 'applications', Applications
 
     desc 'application_versions SUBCOMMAND', 'handle application versions'
     subcommand 'application_versions', ApplicationVersions
