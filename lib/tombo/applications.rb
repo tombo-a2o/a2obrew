6e0ffed37ea1c12d078188eb7c7e378586456389
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-07-16 22:55:04 -0700

    Support application update on tombocli

--- lib/tombo/applications.rb
+++ lib/tombo/applications.rb
@@ -17,5 +17,31 @@ module Tombo
 
       output(json)
     end
+
+    desc 'update', 'applications update'
+    method_option :profile, aliases: '-p', desc: 'Profile name for Tombo Platform'
+    method_option :application_id, desc: 'The ID of the application to be updated', required: true
+    method_option :active_version_id, desc: 'Active Version ID to be updated'
+    def update
+      body = {}
+
+      if options[:active_version_id]
+        body['application[active_version_id]'] = options[:active_version_id]
+      end
+
+      raise 'No column are specified to be updated' if body.empty?
+
+      application_id = options[:application_id]
+
+      json = request('PATCH', "/applications/#{application_id}.json", nil, body)
+
+      d = json['data']
+
+      if d.nil? || d['type'] != 'applications' || d['id'].nil?
+        raise 'Cannot update application'
+      end
+
+      output(json)
+    end
   end
 end
