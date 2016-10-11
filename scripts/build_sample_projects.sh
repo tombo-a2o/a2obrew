d60ec72fc6cd284963786df854936bb3a7706cbe
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-10-10 22:30:07 -0700

    Change the working directory in scripts/build_sample_projects.sh

--- scripts/build_sample_projects.sh
+++ scripts/build_sample_projects.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -exu
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 cd "$parent_path"
+cd ..
 eval "$(bin/a2obrew init -)"
 
-for d in ../sample_projects/*/; do
+for d in sample_projects/*/; do
+  pushd .
   cd "$d"
   make
+  popd
 done
