200df4a45db65c01e32ad8df6e2c510055406217
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2017-02-17 02:10:29 -0800

    Introduce htmlhint

--- shell/lint.sh
+++ shell/lint.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
+#!/bin/sh -exu
 node ../node_modules/eslint/bin/eslint.js --config=.eslintrc --fix shell_files/javascripts/shell.js
+node ../node_modules/htmlhint/bin/htmlhint --config=.htmlhintrc shell.html
