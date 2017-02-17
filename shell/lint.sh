4898aff4f56e07034ecbd21b7c2c8386ce66e2ad
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2017-02-17 02:30:01 -0800

    Introduce csslint

--- shell/lint.sh
+++ shell/lint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -exu
 node ../node_modules/eslint/bin/eslint.js --config=.eslintrc --fix shell_files/javascripts/shell.js
 node ../node_modules/htmlhint/bin/htmlhint --config=.htmlhintrc shell.html
+node ../node_modules/csslint/dist/cli.js --config=.csslintrc shell_files/stylesheets/shell.css
