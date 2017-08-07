293b7f25811dbd96abf6be502bdb39c8ade57fec
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-08-07 19:38:43 +0900

    Link TomboKit framework

--- a2oconfig.rb
+++ a2oconfig.rb
@@ -242,7 +242,7 @@ BUILD
       UIKit Security ImageIO AudioToolbox CommonCrypto SystemConfiguration
       CoreGraphics QuartzCore CFNetwork OpenGLES Onyx2D CoreText
       Social AVFoundation StoreKit CoreFoundation MapKit GameKit MultipeerConnectivity
-      MobileCoreServices
+      MobileCoreServices TomboKit TomboAFNetworking
     ],
     dynamic_link_frameworks: %w[
       Foundation
